ea1d5e9f7eba5b1515509f47ba55022407b25885
Saad <58215768+incognitoprince@users.noreply.github.com>
2026-09-22 18:00:19 +0530

    feat: complete CRM core workflows

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import { LandingPage } from "./pages/LandingPage";
 import { CustomersPage } from "./pages/CustomersPage";
 import { CustomerDetailPage } from "./pages/CustomerDetailPage";
 import { OrdersPage } from "./pages/OrdersPage";
+import { NewOrderPage } from "./pages/NewOrderPage";
+import { OrderDetailPage } from "./pages/OrderDetailPage";
+import { ShopsPage } from "./pages/ShopsPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
 export default function App() {
@@ -16,6 +19,9 @@ export default function App() {
         <Route path="customers" element={<CustomersPage />} />
         <Route path="customers/:id" element={<CustomerDetailPage />} />
         <Route path="orders" element={<OrdersPage />} />
+        <Route path="orders/new" element={<NewOrderPage />} />
+        <Route path="orders/:id" element={<OrderDetailPage />} />
+        <Route path="shops" element={<ShopsPage />} />
         <Route path="home" element={<Navigate to="/dashboard" replace />} />
         <Route path="overview" element={<LandingPage />} />
         <Route path="*" element={<NotFoundPage />} />
